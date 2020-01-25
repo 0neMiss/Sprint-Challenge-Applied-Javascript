77b359d49024dc2680ccd12797f4d67cca2f829a
Jordan <jordanshehane@gmail.com>
2020-01-24 19:35:54 -0700

    components commit

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,28 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+  //calling the promise from axios
+  axios.get('https://lambda-times-backend.herokuapp.com/topics')
+  //creating .then statement to apply the following function to the response object.
+  .then((response) => {
+    for (i = 0; i < response.data.topics.length; i++) {
+      //selecting .topics
+      let topics = document.querySelector('.topics')
+      console.log(response);
+      let topic_element = tabsCreate(response.data.topics[i]);
+
+      topics.appendChild(topic_element);
+    }
+  });
+
+let tabsCreate = (topics) => {
+  //creatting div element
+  let tab = document.createElement('div');
+  //adding classes
+  tab.classList.add('tab');
+  tab.innerHTML = topics;
+  console.log(tab);
+  return tab;
+
+};
